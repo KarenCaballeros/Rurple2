a05707932a96d0ae42599171baca500050d45f42
KarenCaballeros <anakarencaballeros@gmail.com>
2016-09-02 20:44:27 -0600

    arreglos.

--- robot.py
+++ robot.py
@@ -18,15 +18,43 @@ class Robot(object):
 				if j == y and i == x:
 					return 
 
-	def mover():
-	
+	def rotar(self):
+		if self.direccion == "UP":
+			self.direccion = "RIGHT"
+		elif self.direccion == "RIGHT":
+			self.direccion = "DOWN"
+		elif self.direccion == "DOWN":
+			self.direccion == "LEFT"
+		else:
+			self.direccion = "UP"
 
-	def rotar():
+	def mover(self):
+		if self.direccion == "UP":
+			self.x -= 1
+		elif self.direccion == "RIGHT":
+			self.y += 1
+		elif self.direccion == "DOWN":
+			self.x += 1
+		else:
+			self.y -= 1
+		
+		if self.x <= 0:
+			self.x = 0
+		elif self.y <= 0:
+			self.y = 0
+		elif self.x >= ancho:
+			self.x = ancho
+		elif self.y >= altura:
+			self.y = altura			 	
 
 
-	def recoger():		
+	def recoger(self):		
+		if self.x == self.monedas.x and self.y == self.monedas.y:
+			self.monedas +=1
+			break
 
-	def sumar_monedas():
+
+	def sumar_monedas(self):
 		if self.monedas		
 
 			
